768564868345bc436457b4372a9c28fb0021034b
godninoff <godninoff21@yandex.ru>
2021-08-29 20:53:20 +0500

    убрал корс для проверки

--- backend/app.js
+++ backend/app.js
@@ -24,7 +24,7 @@ mongoose.connect('mongodb://localhost:27017/mestodb', {
   useUnifiedTopology: true,
 });
 
-app.use(cors);
+// app.use(cors);
 app.use(helmet());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
