829f0975a09d186e91faa3f1e37f1a312902a529
godninoff <godninoff21@yandex.ru>
2021-08-29 20:57:59 +0500

    http

--- backend/app.js
+++ backend/app.js
@@ -24,7 +24,7 @@ mongoose.connect('mongodb://localhost:27017/mestodb', {
   useUnifiedTopology: true,
 });
 
-// app.use(cors);
+app.use(cors);
 app.use(helmet());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
@@ -32,7 +32,11 @@ app.use(cookieParser());
 
 app.use(requestLogger);
 
-
+app.get('/crash-test', () => {
+  setTimeout(() => {
+    throw new Error('Сервер сейчас упадёт');
+  }, 0);
+});
 
 app.post('/signup', celebrate({
   body: Joi.object().keys({
--- backend/middlewares/cors.js
+++ backend/middlewares/cors.js
@@ -1,25 +1,25 @@
-// const DEFAULT_ALLOWED_METHODS = 'GET,HEAD,PUT,PATCH,POST,DELETE';
-// const ALLOWED_CORS = [
-//   'https://strannoe.mesto.nomoredomains.work',
-//   'https://api.weirdplace.students.nomoredomains.club',
-//   'http://localhost:3000'
-// ];
+const DEFAULT_ALLOWED_METHODS = 'GET,HEAD,PUT,PATCH,POST,DELETE';
+const ALLOWED_CORS = [
+  'strannoe.mesto.nomoredomains.work',
+  'api.weirdplace.students.nomoredomains.club',
+  'localhost:3000'
+];
 
-// module.exports = (req, res, next) => {
-//   const { method } = req;
-//   const requestHeaders = req.headers['access-control-request-headers'];
-//   const { origin } = req.headers;
+module.exports = (req, res, next) => {
+  const { method } = req;
+  const requestHeaders = req.headers['access-control-request-headers'];
+  const { origin } = req.headers;
 
-//   if (ALLOWED_CORS.includes(origin)) {
-//     res.header('Access-Control-Allow-Origin', origin);
-//     res.header('Access-Control-Allow-Credentials', 'true');
-//   }
+  if (ALLOWED_CORS.includes(origin)) {
+    res.header('Access-Control-Allow-Origin', origin);
+    res.header('Access-Control-Allow-Credentials', 'true');
+  }
 
-//   if (method === 'OPTIONS') {
-//     res.header('Access-Control-Allow-Methods', DEFAULT_ALLOWED_METHODS);
-//     res.header('Access-Control-Allow-Headers', requestHeaders);
-//     return res.end();
-//   }
+  if (method === 'OPTIONS') {
+    res.header('Access-Control-Allow-Methods', DEFAULT_ALLOWED_METHODS);
+    res.header('Access-Control-Allow-Headers', requestHeaders);
+    return res.end();
+  }
 
-//   next();
-// };
+  next();
+};
